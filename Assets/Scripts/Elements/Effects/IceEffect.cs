11e021caa707ec85d34322df27afb627e05250ea
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2015-05-18 15:09:22 +0200

    Fixes IceEffect

--- Assets/Scripts/Elements/Effects/IceEffect.cs
+++ Assets/Scripts/Elements/Effects/IceEffect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class IceEffect : IShotEffect
 {
     #region Members
-    [Range(0, 100)]
+    [Range(0.0f, 100.0f)]
     public float slowRate = 10.0f;
     public float duration = 10.0f;
     public float interval = 1.0f;
@@ -12,10 +12,14 @@ public class IceEffect : IShotEffect
 
     public IEnumerator applyEffect(Enemy enemyScript)
     {
-        enemyScript.alterSpeedRate(-slowRate);
+        // Carefu
+        slowRate /= 100.0f;
+        slowRate = -slowRate;
+
+        enemyScript.alterSpeedRate(slowRate);
 
         yield return new WaitForSeconds(interval);
 
-        enemyScript.revertSpeedRate(-slowRate);
+        enemyScript.revertSpeedRate(slowRate);
     }
 }
