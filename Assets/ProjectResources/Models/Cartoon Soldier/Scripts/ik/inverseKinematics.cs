561d2676e01b33dae1abccc54488969a7988f84b
Costa Florent <costa_florent@hotmail.fr>
2015-05-20 02:21:35 +0200

    inversekinematics script

---
+++ Assets/ProjectResources/Models/Cartoon Soldier/Scripts/ik/inverseKinematics.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inverseKinematics : MonoBehaviour
+{
+    private Vector3 target;
+    private Vector3 elbowTarget;
+
+    public void CalculateIK()
+    {
+        transform.LookAt(target, transform.position - elbowTarget);
+        Transform upperArm = transform.Find("upperArm");
+        Transform elbow = transform.Find("upperArm/elbow");
+        Transform hand = transform.Find("upperArm/elbow/hand");
+        float upperArmLength = Vector3.Distance(upperArm.position, elbow.position);
+        float forearmLength = Vector3.Distance(elbow.position, hand.position);
+        float armLength = upperArmLength + forearmLength;
+        float hypotenuse = upperArmLength;
+        float targetDistance = Vector3.Distance(upperArm.position, target);
+        targetDistance = Mathf.Min(targetDistance, armLength - 0.0001f); //Do not allow target distance be further away than the arm's length.
+        float adjacent = targetDistance * (upperArmLength / armLength);
+        float ikAngle = Mathf.Acos(adjacent / hypotenuse) * Mathf.Rad2Deg;
+        upperArm.LookAt(target, transform.root.up);
+        Vector3 temp = upperArm.localRotation.eulerAngles;
+        upperArm.localRotation = Quaternion.Euler(temp.x + ikAngle, temp.y, temp.z);
+        elbow.LookAt(target, transform.root.up);
+    }
+}
